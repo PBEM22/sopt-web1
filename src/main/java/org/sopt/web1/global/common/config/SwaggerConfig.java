a6a0cfef7642b4c33daeb29be8f96c5aa046aa2a
pbem22 <dlacofbs1229@gmail.com>
2025-11-21 01:45:17 +0900

    chore: #2 초기 환경 세팅

---
+++ src/main/java/org/sopt/web1/global/common/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.sopt.web1.global.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SOPT Web1 API 명세서")
+                        .description("SOPT 37기 Web1팀 API 명세서입니다.")
+                        .version("1.0.0"));
+    }
+}
--- src/main/java/org/sopt/web1/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/web1/global/exception/GlobalExceptionHandler.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> handleBusinessException(BusinessException e) {
+    public ResponseEntity<ApiResponse<?>> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ApiResponse<Void> response = ApiResponse.fail(errorCode.getCode(), errorCode.getMsg());
 
@@ -21,8 +22,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(errorCode.getStatus()).body(response);
     }
 
+    /**
+     * 404 (NoHandlerFoundException) 핸들링
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        ApiResponse<Void> response = ApiResponse.fail(
+                HttpStatus.NOT_FOUND.value(),
+                ErrorCode.NOT_FOUND_URL.getMsg()
+        );
+
+        log.error("Not Found URL: {}", e.getMessage(), e);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception e) {
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
 
         ApiResponse<Void> response = ApiResponse.fail(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
